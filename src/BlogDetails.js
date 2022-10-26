e9b6c5cf60c391d1726e9dc9812cd984f2cf1854
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-10-26 13:46:15 +0100

    Deleting blogs

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,5 +1,8 @@
 import { useParams } from 'react-router-dom/cjs/react-router-dom';
 import useFetch from './useFetch';
+import { useHistory } from 'react-router-dom';
+
+// import { useHistory } from 'react-router-dom ';
 
 const BlogDetails = () => {
   const { id } = useParams();
@@ -8,6 +11,16 @@ const BlogDetails = () => {
     error,
     isPending,
   } = useFetch(`http://localhost:8000/blogs/${id}`);
+
+  const history = useHistory();
+
+  const handleClick = () => {
+    fetch(`http://localhost:8000/blogs/${blog.id}`, {
+      method: 'DELETE',
+    }).then(() => {
+      history.push('/');
+    });
+  };
 
   return (
     <div className="blog-details">
@@ -18,6 +31,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>
