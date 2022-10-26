942834000c3923a801ab99a941effc911085a8f6
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-10-26 12:47:44 +0100

    Submit Events

--- src/Create.js
+++ src/Create.js
@@ -5,10 +5,16 @@ const Create = () => {
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+    console.log(blog);
+  };
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
           type="text"
@@ -30,10 +36,6 @@ const Create = () => {
           <option value="yoshi">Yoshi</option>
         </select>
         <button>Add Blog</button>
-
-        <p>{title}</p>
-        <p>{body}</p>
-        <p>{author}</p>
       </form>
     </div>
   );
